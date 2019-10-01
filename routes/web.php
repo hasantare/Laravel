5c59c4871f839d11f6758c4591d8958eb9a11165
hasan.tareh <hasan.tareh@gmail.com>
2019-10-01 18:37:55 +0330

    first commit on Laravel project

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('students', function () {
+    return view('students');
+});
+Route::get('class', function () {
+    return view('class');
+});
+Route::get('course', function () {
+    return view('course');
+});
